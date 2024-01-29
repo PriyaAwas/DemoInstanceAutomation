d375431ca6ba32fd02fe1b236bff650785eb9d55
Neethu <neethu.sharma@pwc.com>
2024-01-29 12:59:15 +0530

    Test script for guest user.

--- src/test/java/demo/tests/scp/GuestUserTests.java
+++ src/test/java/demo/tests/scp/GuestUserTests.java
@@ -2,23 +2,16 @@ package demo.tests.scp;
 
 import java.io.IOException;
 import java.sql.SQLException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
-import demo.steps.scp.AboutMyHomeSteps;
 import demo.steps.scp.GuestUserSteps;
 import sew.ai.config.Configuration;
-import sew.ai.config.SCPConfiguration;
 import sew.ai.enums.CategoryType;
 import sew.ai.helpers.anotations.FrameworkAnnotations;
-import sew.ai.helpers.testrail.TestRail;
-import sew.ai.models.User;
 import sew.ai.runner.TestRunner;
 import sew.ai.steps.scp.DashboardSteps;
-import sew.ai.steps.scp.GuestuserSteps;
 import sew.ai.steps.scp.HomeSteps;
 import sew.ai.steps.scp.LoginSteps;
 
@@ -26,7 +19,7 @@ public class GuestUserTests extends TestRunner {
 	private static final Logger log = LogManager.getLogger(GuestUserTests.class);
 	private GuestUserSteps guestuserSteps;
 
-	@FrameworkAnnotations(author = { "Neethu" }, category = { CategoryType.SANITY })
+	@FrameworkAnnotations(author = { "Neethu" }, category = { CategoryType.SCP_MYACCOUNT })
 	@Test(description = "To verify Guest User page UI.")
 	public void verifyGuestUserPageUI() throws SQLException, InterruptedException {
 		SoftAssert softAssert = new SoftAssert();
@@ -45,7 +38,7 @@ public class GuestUserTests extends TestRunner {
 		log.info("Test case execution for - verifyGuestUserPageUI - is completed.");
 	}
 
-	@FrameworkAnnotations(author = { "Neethu" }, category = { CategoryType.SANITY })
+	@FrameworkAnnotations(author = { "Neethu" }, category = { CategoryType.SCP_MYACCOUNT })
 	@Test(description = "To verify invite new guest user functionality.")
 	public void verifyInviteNewGuestUserFunctionnality() throws InterruptedException, IOException {
 		SoftAssert softAssert = new SoftAssert();
@@ -62,6 +55,26 @@ public class GuestUserTests extends TestRunner {
 		// Assert all the soft verification.
 		softAssert.assertAll();
 		log.info("Test case execution for - verifyInviteNewGuestUserFunction - is completed.");
+	}
+
+	@FrameworkAnnotations(author = { "Neethu" }, category = { CategoryType.SCP_MYACCOUNT })
+	@Test(description = "To verify resend activation link functionality.")
+	public void verifyResendActivationLinkFunction() throws SQLException, InterruptedException, IOException {
+		SoftAssert softAssert = new SoftAssert();
+		// Init login steps
+		LoginSteps loginSteps = new LoginSteps(driver);
+		DashboardSteps dashboardSteps = loginSteps.loginIntoTheApplication(Configuration.toString("userName"),
+				Configuration.toString("password"));
+		HomeSteps homeSteps = new HomeSteps(driver);
+		homeSteps.navigateToGuestUser();
+		// into AboutMyHomePageSteps
+		guestuserSteps = new GuestUserSteps(driver);
+		// Call verifyResendActivationLinkFunction Method
+		guestuserSteps.verifyResendActivationLinkFunction(softAssert);
+		// Assert all the soft verification.
+		softAssert.assertAll();
+		log.info("Test case execution for - verifyResendActivationLinkFunction - is completed.");
+
 	}
 
 //	@FrameworkAnnotations(author = { "Neethu" }, category = { CategoryType.SANITY })
@@ -82,24 +95,4 @@ public class GuestUserTests extends TestRunner {
 //		softAssert.assertAll();
 //		log.info("Test case execution for - verifyEditGuestDetailsFunction - is completed.");
 //	}
-
-	@FrameworkAnnotations(author = { "Neethu" }, category = { CategoryType.SANITY })
-	@Test(description = "To verify resend activation link functionality.")
-	public void verifyResendActivationLinkFunction() throws SQLException, InterruptedException, IOException {
-		SoftAssert softAssert = new SoftAssert();
-		// Init login steps
-		LoginSteps loginSteps = new LoginSteps(driver);
-		DashboardSteps dashboardSteps = loginSteps.loginIntoTheApplication(Configuration.toString("userName"),
-				Configuration.toString("password"));
-		HomeSteps homeSteps = new HomeSteps(driver);
-		homeSteps.navigateToGuestUser();
-		// into AboutMyHomePageSteps
-		guestuserSteps = new GuestUserSteps(driver);
-		// Call verifyResendActivationLinkFunction Method
-		guestuserSteps.verifyResendActivationLinkFunction(softAssert);
-		// Assert all the soft verification.
-		softAssert.assertAll();
-		log.info("Test case execution for - verifyResendActivationLinkFunction - is completed.");
-
-	}
 }
