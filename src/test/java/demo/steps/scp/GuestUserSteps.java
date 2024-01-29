c30278678ee85ad1bbd62c7e0e2eaa5714c5bce2
Neethu <neethu.sharma@pwc.com>
2024-01-29 12:17:56 +0530

    Test Script for Guest User

---
+++ src/test/java/demo/steps/scp/GuestUserSteps.java
@@ -0,0 +1,182 @@
+package demo.steps.scp;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+import sew.ai.config.CSPConfiguration;
+import sew.ai.config.Configuration;
+import sew.ai.config.SCPConfiguration;
+import sew.ai.driver.DriverFactory;
+import sew.ai.driver.Page;
+import sew.ai.helpers.props.Constants;
+import sew.ai.helpers.props.FilePaths;
+import sew.ai.helpers.props.SQLQueries;
+import sew.ai.helpers.reporters.ExtentLogger;
+import sew.ai.models.RegistrationConfig;
+import sew.ai.models.User;
+import sew.ai.pageObjects.scp.AccountInformationPage;
+import sew.ai.pageObjects.scp.BillDashboardPage;
+import sew.ai.pageObjects.scp.DashboardPage;
+import demo.pageobjects.GuestUserPage;
+import sew.ai.pageObjects.scp.LoginPage;
+import sew.ai.pageObjects.scp.MultiFactorAuthPage;
+import sew.ai.pageObjects.scp.MyProfilePage;
+import sew.ai.pageObjects.scp.RegistrationPage;
+import sew.ai.steps.scp.HomeSteps;
+import sew.ai.steps.scp.LoginSteps;
+import sew.ai.steps.scp.RegistrationSteps;
+import sew.ai.utils.DataBaseUtils;
+import sew.ai.utils.DateUtil;
+import sew.ai.utils.JsonUtil;
+import sew.ai.utils.PropertiesUtil;
+import sew.ai.utils.RandomUtil;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GuestUserSteps extends GuestUserPage {
+	private static final Logger log = LogManager.getLogger(GuestUserSteps.class);
+	public static PropertiesUtil guestuserTextProp;
+
+	public GuestUserSteps(WebDriver driver) {
+		super(driver);
+		guestuserTextProp = new PropertiesUtil(FilePaths.SCP_TEXT_PROPERTIES + Constants.GUEST_USER_TEXT_FILENAME);
+	}
+
+	public Boolean isGuestUserPage(String url, String title) {
+		Boolean isguestuserPage = false;
+		log.info("Checking that the current page is guest user page.");
+		if (getCurrentUrl().contains(url.toLowerCase()) && getCurrentTitle().equalsIgnoreCase(title))
+			isguestuserPage = true;
+		log.info("The current page is guest user page {}: " + isguestuserPage);
+		return isguestuserPage;
+	}
+
+	public void verifyGuestUserPageUI(SoftAssert softAssert) throws SQLException {
+		pause(5000);
+
+		if (getListTabsGuestUserDetails().size() > 0) {
+
+			ExtentLogger.logInfo("Verify Guest User Page UI");
+			softAssert.assertTrue(isPageHeadVisible(), "Page Title is not visible");
+			softAssert.assertTrue(isInviteGuestUserSecVisible(), "Invite Guest User Section is not visible");
+			softAssert.assertTrue(isExistGuestUserSecVisible(), "Existing Guest User Section is not visible");
+			softAssert.assertTrue(isGuestUserBannerVisible(), "Guest User Banner is not visible");
+			softAssert.assertTrue(isContactUsBannerVisible(), "Contact Us Banner is not visible");
+		}
+	}
+
+	public void inviteNewGuestUserFunctionality(SoftAssert softAssert) throws IOException {
+
+		deleteGuestUser(softAssert);
+
+		ExtentLogger.logInfo("Verify that customer is able to click invite new guest user section successfully.");
+		clickOnInviteUser();
+		ExtentLogger
+				.logInfo("Verify that customer is able to click on the account number drop down field successfully.");
+		clickAccountDropDown();
+		ExtentLogger.logInfo("Verify that customer is able to select the account number successfully.");
+		selectServiceAccountnumber();
+		pause(2000);
+		ExtentLogger.logInfo("Verify that customer is able to populate the guest user name field successfully.");
+		populateGuestUserName("Guest One");
+		pause(500);
+		ExtentLogger.logInfo("Verify that customer is able to select the guest user role successfully.");
+		selectGuestUserRole();
+		pause(500);
+		ExtentLogger.logInfo("Verify that customer is able to populate the guest user emailid field successfully.");
+		populateGuestUserEmailId("guest.one@testmail.com");
+		ExtentLogger.logInfo("Verify that customer is able to select access period successfully.");
+		isAccessPeriodClickable();
+		ExtentLogger.logInfo("Verify that customer is able to select check box successfully.");
+		checkchkBoxAgreeTeBtn();
+		ExtentLogger.logInfo("Verify that customer is able to click on submit button successfully.");
+		clickSubmitButton();
+		softAssert.assertEquals(getInviteGuestUserSuccessLabel(),
+				guestuserTextProp.getPropValue("txtMsgSuccessfulGuestInvite"));
+	}
+
+	public void deleteGuestUser(SoftAssert softAssert) {
+		pause(1000);
+		List<WebElement> btnToggleThreeDots = getListBtnToggleThreeDots();
+		int size = btnToggleThreeDots.size();
+		for (int i = 0; i < size; i++) {
+			// Verifying delete/revoke guest access functionality.
+			pause(2000);
+			clickWithJSExecutor(getListBtnToggleThreeDots().get(0));
+			clickWithJSExecutor(getBtnRemoveGuestAccount().get(0));
+			pause(5000);
+			clickContinueConfirmationMsgBtn();
+			String expRevokeConfirmationMsg = guestuserTextProp.getPropValue("txtLblRevokeConfirmationMsg");
+			String actRevokeConfirmationMsg = getConfirmationPopupMsg();
+			softAssert.assertEquals(actRevokeConfirmationMsg, expRevokeConfirmationMsg,
+					"Revoke confirmation message is not matched.");
+			// pause(1000);
+			// clickContinueConfirmationMsgBtn();
+			pause(10000);
+		}
+	}
+
+	public void verifyEditGuestDetailsFunction(SoftAssert softAssert) throws IOException, SQLException {
+
+		deleteGuestUser(softAssert);
+
+		inviteNewGuestUserFunctionality(softAssert);
+
+		String accountNo = Configuration.toString("scmAccountNumber");
+		getBtnThreeDots(accountNo);
+
+		if (isexistinguserdisplayed()) {
+
+			ExtentLogger.logInfo("Verify that customer is able to click three dot icon successfully.");
+			clickThreeDotIconGuestBtn();
+			ExtentLogger.logInfo("Verify that customer is able to select edit option successfully.");
+			clickEditGuestBtn();
+			pause(500);
+			ExtentLogger.logInfo("Verify that customer is able to edit the guest user role successfully.");
+			selectGuestUserRole();
+			pause(500);
+			ExtentLogger.logInfo("Verify that customer is able to select check box successfully.");
+			selectchkBoxOptn();
+			ExtentLogger.logInfo("Verify that customer is able to click on submit button successfully.");
+			clickSubmitButton();
+			softAssert.assertEquals(getDetailsUpdatedLabel(),
+					guestuserTextProp.getPropValue("txtMsgSuccessfullUpdateGup"));
+		} else {
+			ExtentLogger.logInfo("Existing guest user not available.");
+		}
+	}
+
+	public void verifyResendActivationLinkFunction(SoftAssert softAssert) throws IOException, SQLException {
+
+		String accountNo = Configuration.toString("scmAccountNumber");
+		getBtnThreeDots(accountNo);
+
+		softAssert.assertTrue(btnResendInvitationIcon(accountNo).isDisplayed(),
+				"Resend activation link is not displaying before approving guest user request.");
+		if (btnResendInvitationIcon(accountNo).isDisplayed()) {
+			String invitationStatus = getStatusOfInvitation(accountNo);
+			String expInvitationStatus = guestuserTextProp.getPropValue("txtLblInvitationPendingStatus");
+			softAssert.assertEquals(invitationStatus, expInvitationStatus, "Invitation status not matched.");
+			String expToastMsg = guestuserTextProp.getPropValue("txtLblResendInvitationToastMsg");
+			clickBtnResendInvitationIcon(accountNo);
+			String actToastMsg = getToastMessage();
+			softAssert.assertEquals(actToastMsg, expToastMsg, "Resend activation link toast message not matched");
+		} else {
+			softAssert.assertTrue(btnResendInvitationIcon(accountNo).isDisplayed(),
+					"Resend activation link is not displaying before approving guest user request.");
+		}
+		log.info("To verify resend the activation functionality from their guest user tab.");
+		log.info("Test Case execution for - verifyResendActivationLinkFunctionality - is Completed");
+	}
+
+}
---
+++ src/test/java/demo/tests/scp/GuestUserTests.java
@@ -0,0 +1,105 @@
+package demo.tests.scp;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import demo.steps.scp.AboutMyHomeSteps;
+import demo.steps.scp.GuestUserSteps;
+import sew.ai.config.Configuration;
+import sew.ai.config.SCPConfiguration;
+import sew.ai.enums.CategoryType;
+import sew.ai.helpers.anotations.FrameworkAnnotations;
+import sew.ai.helpers.testrail.TestRail;
+import sew.ai.models.User;
+import sew.ai.runner.TestRunner;
+import sew.ai.steps.scp.DashboardSteps;
+import sew.ai.steps.scp.GuestuserSteps;
+import sew.ai.steps.scp.HomeSteps;
+import sew.ai.steps.scp.LoginSteps;
+
+public class GuestUserTests extends TestRunner {
+	private static final Logger log = LogManager.getLogger(GuestUserTests.class);
+	private GuestUserSteps guestuserSteps;
+
+	@FrameworkAnnotations(author = { "Neethu" }, category = { CategoryType.SANITY })
+	@Test(description = "To verify Guest User page UI.")
+	public void verifyGuestUserPageUI() throws SQLException, InterruptedException {
+		SoftAssert softAssert = new SoftAssert();
+		// Init login steps
+		LoginSteps loginSteps = new LoginSteps(driver);
+		DashboardSteps dashboardSteps = loginSteps.loginIntoTheApplication(Configuration.toString("userName"),
+				Configuration.toString("password"));
+		HomeSteps homeSteps = new HomeSteps(driver);
+		homeSteps.navigateToGuestUser();
+		// into AboutMyHomePageSteps
+		guestuserSteps = new GuestUserSteps(driver);
+		// Call verifyGuestUserPageUI Method
+		guestuserSteps.verifyGuestUserPageUI(softAssert);
+		// Assert all the soft verification.
+		softAssert.assertAll();
+		log.info("Test case execution for - verifyGuestUserPageUI - is completed.");
+	}
+
+	@FrameworkAnnotations(author = { "Neethu" }, category = { CategoryType.SANITY })
+	@Test(description = "To verify invite new guest user functionality.")
+	public void verifyInviteNewGuestUserFunctionnality() throws InterruptedException, IOException {
+		SoftAssert softAssert = new SoftAssert();
+		// Init login steps
+		LoginSteps loginSteps = new LoginSteps(driver);
+		DashboardSteps dashboardSteps = loginSteps.loginIntoTheApplication(Configuration.toString("userName"),
+				Configuration.toString("password"));
+		HomeSteps homeSteps = new HomeSteps(driver);
+		homeSteps.navigateToGuestUser();
+		// into AboutMyHomePageSteps
+		guestuserSteps = new GuestUserSteps(driver);
+		// Call verifyInviteNewGuestUserFunction Method
+		guestuserSteps.inviteNewGuestUserFunctionality(softAssert);
+		// Assert all the soft verification.
+		softAssert.assertAll();
+		log.info("Test case execution for - verifyInviteNewGuestUserFunction - is completed.");
+	}
+
+//	@FrameworkAnnotations(author = { "Neethu" }, category = { CategoryType.SANITY })
+//	@Test(description = "To verify edit guest user functionality.")
+//	public void verifyEditGuestDetailsFunction() throws SQLException, InterruptedException, IOException {
+//		SoftAssert softAssert = new SoftAssert();
+//		// Init login steps
+//		LoginSteps loginSteps = new LoginSteps(driver);
+//		DashboardSteps dashboardSteps = loginSteps.loginIntoTheApplication(Configuration.toString("userName"),
+//				Configuration.toString("password"));
+//		HomeSteps homeSteps = new HomeSteps(driver);
+//		homeSteps.navigateToGuestUser();
+//		// into AboutMyHomePageSteps
+//		guestuserSteps = new GuestUserSteps(driver);
+//		// Call verifyEditGuestDetailsFunction Method
+//		guestuserSteps.verifyEditGuestDetailsFunction(softAssert);
+//		// Assert all the soft verification.
+//		softAssert.assertAll();
+//		log.info("Test case execution for - verifyEditGuestDetailsFunction - is completed.");
+//	}
+
+	@FrameworkAnnotations(author = { "Neethu" }, category = { CategoryType.SANITY })
+	@Test(description = "To verify resend activation link functionality.")
+	public void verifyResendActivationLinkFunction() throws SQLException, InterruptedException, IOException {
+		SoftAssert softAssert = new SoftAssert();
+		// Init login steps
+		LoginSteps loginSteps = new LoginSteps(driver);
+		DashboardSteps dashboardSteps = loginSteps.loginIntoTheApplication(Configuration.toString("userName"),
+				Configuration.toString("password"));
+		HomeSteps homeSteps = new HomeSteps(driver);
+		homeSteps.navigateToGuestUser();
+		// into AboutMyHomePageSteps
+		guestuserSteps = new GuestUserSteps(driver);
+		// Call verifyResendActivationLinkFunction Method
+		guestuserSteps.verifyResendActivationLinkFunction(softAssert);
+		// Assert all the soft verification.
+		softAssert.assertAll();
+		log.info("Test case execution for - verifyResendActivationLinkFunction - is completed.");
+
+	}
+}
