25af9aebcffdb3588d9c4e6cf038308c83776c53
EujinGG <eujin.j.george@pwc.com>
2024-01-30 12:07:29 +0530

    refactored prelogin and preloginoutages

--- src/test/java/demo/tests/scp/PreLoginTests.java
+++ src/test/java/demo/tests/scp/PreLoginTests.java
@@ -22,21 +22,21 @@ public class PreLoginTests extends TestRunner {
 	private static final Logger log = LogManager.getLogger(PreLoginTests.class);
 	private PreLoginSteps preLoginSteps;
 	public static PropertiesUtil loginTextProp;
-	String accountType = null;
 	
-	@FrameworkAnnotations(author = { "Eujin" }, category = { CategoryType.SANITY,CategoryType.SCP_LOGIN })
+	@FrameworkAnnotations(author = { "Eujin" }, category = { CategoryType.SMOKE })
 	@Test(priority = 1, description = "User is able to Login with valid credentials.")
 	public void verifyValidLogin() {
-		log.info("To Verify Payment information feilds and Add payment method pop-up");
+		log.info("To Verify Payment information fields and Add payment method pop-up");
 		SoftAssert softAssert = new SoftAssert();
 		//SCP- Application Login
 		preLoginSteps = new PreLoginSteps(driver);
 		preLoginSteps.loginIntoTheApplication(Configuration.toString("userName"), Configuration.toString("password"));
 		softAssert.assertAll();
+        ExtentLogger.logInfo("Test Case execution for - verifyValidLogin - is Completed.");
 	}
 	
 
-    @FrameworkAnnotations(author = {"Eujin"}, category = {CategoryType.SANITY})
+    @FrameworkAnnotations(author = {"Eujin"}, category = {CategoryType.SMOKE})
     @Test(priority = 3, description = "To verify the login functionality with invalid credentials.")
     public void verifyInvalidLogin() {
         log.info("To verify the tests with the below test case id's: " +
@@ -48,11 +48,12 @@ public class PreLoginTests extends TestRunner {
         // Assert all the soft verification.
         softAssert.assertAll();
         log.info("Test Case execution for - verifyInvalidLogin - is Completed.");
+        ExtentLogger.logInfo("Test Case execution for - verifyInvalidLogin - is Completed.");
     }
     
    
-    @FrameworkAnnotations(author = {"Eujin"}, category = {CategoryType.SANITY})
-    @Test(priority = 1, description = "To verify the login page objects.", groups = {"sanity"})
+    @FrameworkAnnotations(author = {"Eujin"}, category = {CategoryType.SMOKE})
+    @Test(priority = 1, description = "To verify the login page objects.")
     public void verifyLoginPageObjects() {
         log.info("To verify the tests with the below test case id's: " +
                 "C75080, C101604");
@@ -64,10 +65,11 @@ public class PreLoginTests extends TestRunner {
         // Assert all the soft verification.
         softAssert.assertAll();
         log.info("Test Case execution for - verifyLoginPageObjects - is Completed.");
+        ExtentLogger.logInfo("Test Case execution for - verifyLoginPageObjects - is Completed.");
     }
     
     
-    @FrameworkAnnotations(author = {"Eujin"}, category = {CategoryType.SANITY})
+    @FrameworkAnnotations(author = {"Eujin"}, category = {CategoryType.SMOKE})
     @Test(priority = 4, description = "To verify the remember me login functionality.")
     public void verifyRememberMeLoginFunctionality() {
    
@@ -91,10 +93,11 @@ public class PreLoginTests extends TestRunner {
         // Assert all the soft verification.
         softAssert.assertAll();
         log.info("Test Case execution for - verifyRememberMeLoginFunctionality - is Completed.");
+        ExtentLogger.logInfo("Test Case execution for - verifyRememberMeLoginFunctionality - is Completed.");
     }
     
     
-    @FrameworkAnnotations(author = {"Eujin"}, category = {CategoryType.SANITY})
+    @FrameworkAnnotations(author = {"Eujin"}, category = {CategoryType.SMOKE})
     @Test(priority = 5, description = "To verify the Pre-Login Connect Me page.")
     public void verifyPreLogTrackContactReq() {
     	log.info("Test Case execution for - verifyPreLoginConnectMePage - is Initiated");
@@ -110,10 +113,11 @@ public class PreLoginTests extends TestRunner {
         // Submit Form
         referenceId = preLoginSteps.verifyPreConnectMeSubmitForm(softAssert);
         log.info("reference ID: " + referenceId);
-       log.info("Test Case execution for - verifyPostLoginConnectMePage - is Completed.");
+       log.info("Test Case execution for - verifyPreLogTrackContactReq - is Completed.");
+        ExtentLogger.logInfo("Test Case execution for - verifyPreLogTrackContactReq - is Completed.");
      }
     
-    @FrameworkAnnotations(author = {"Eujin"}, category = {CategoryType.SANITY})
+    @FrameworkAnnotations(author = {"Eujin"}, category = {CategoryType.SMOKE})
     @Test(priority = 5, description = "Verify User is able to Submit a Contact Us Request form for Water waste, theft without logging in.")
     public void verifyPreLogTrackContactReqForWaterWaste() {
     	log.info("Test Case execution for - verifyPreLoginConnectMePageForWaterWaste - is Initiated");
@@ -130,10 +134,11 @@ public class PreLoginTests extends TestRunner {
         referenceId = preLoginSteps.verifyPreConnectMeSubmitForm(softAssert);
         log.info("reference ID: " + referenceId);
         ExtentLogger.logInfo("Reference Id: " + referenceId);
-       log.info("Test Case execution for - verifyPostLoginConnectMePage - is Completed.");
+       log.info("Test Case execution for - verifyPreLogTrackContactReqForWaterWaste - is Completed.");
+        ExtentLogger.logInfo("Test Case execution for - verifyPreLogTrackContactReqForWaterWaste - is Completed.");
      }
     
-    @FrameworkAnnotations(author = { "Kavya BR" }, category = { CategoryType.SANITY,CategoryType.SCP_LOGIN })
+    @FrameworkAnnotations(author = { "Kavya BR" }, category = { CategoryType.SMOKE })
 	@Test(priority = 1, description = "To verify chat functionality.")
 	public void verifyPreLogChatBox()throws SQLException {
 		log.info("To Verify Payment information feilds and Add payment method pop-up");
@@ -142,16 +147,18 @@ public class PreLoginTests extends TestRunner {
 		preLoginSteps = new PreLoginSteps(driver);
 		preLoginSteps.verifyPreLogChatBox(softAssert);
 		softAssert.assertAll();
+        ExtentLogger.logInfo("Test Case execution for - verifyPreLogChatBox - is Completed.");
 	}
 
-    @FrameworkAnnotations(author = { "Kavya BR" }, category = { CategoryType.SANITY,CategoryType.SCP_LOGIN })
+    @FrameworkAnnotations(author = { "Kavya BR" }, category = { CategoryType.SMOKE })
 	@Test(priority = 1, description = "User is able to Login with valid credentials.")
 	public void verifySignOutPage() {
-		log.info("To Verify Payment information feilds and Add payment method pop-up");
+		log.info("To Verify Payment information fields and Add payment method pop-up");
 		SoftAssert softAssert = new SoftAssert();
 		//SCP- Application Login
 		preLoginSteps = new PreLoginSteps(driver);
 		preLoginSteps.loginIntoTheApplication(Configuration.toString("userName"), Configuration.toString("password"));
 		softAssert.assertAll();
+        ExtentLogger.logInfo("Test Case execution for - verifySignOutPage - is Completed.");
 	}
 }
